5c5fc93b76c3df616e10378aa3c32cca1a98990c
norah seini kinyamasyo <norahkinyamasyo@moringaschool.com>
2024-07-03 23:26:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat (bob){
+    cats.push(bob);
+}
+
+function destructivelyPrependCat (bobby) {
+    cats.unshift(bobby);
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat (){
+    cats.shift();
+}
+
+function appendCat(knight){
+    return [...cats, knight];
+}
+
+function prependCat(knight){
+    return [knight, ...cats];
+}
+
+function removeLastCat(){
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
